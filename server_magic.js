1360e4c152d7ae42b315f237506d7698363802f3
Aaron Brooks <rabrooks@multiservice.com>
2013-11-27 09:45:11 -0600

    Added stdout commands

--- server_magic.js
+++ server_magic.js
@@ -2,19 +2,23 @@
      sys = require('sys'),
      exec = require('child_process').exec,
      watch = require('./watch/main.js'),
-     config = 'config.json',
+     config = '/home/rabrooks/msc-task-runner/config.json',
+     server_start = 'httpd/start',
+     server_stop = 'httpd/stop',
      data;
 
   function serverTask(error, stdout, stderr) {
-    sys.puts(stdout);
+    if (stdout) {
+      sys.puts(stdout);
+    } else if (stderr) {
+      sys.puts(stderr);
+    } else if (error) {
+      sys.puts(error);
+    }
+    console.log('The Command: ' + stdout + " Should Have Executed.")
   }
-
-  function minifyTask() {
-    console.log('Running Minify');
-  }
-
-  exec("ls -a", serverTask);
-  //serverTask('start');
+  exec('newgrp msfleet', serverTask );
+  exec(server_start, serverTask);
   data = JSON.parse(fs.readFileSync(config));
 
   if (data.username) {
@@ -27,55 +31,60 @@
     var user = data.username,
         perl_path = data.perl_path,
         javascript_path = data.javascript_path,
-        css_path = data.css_path,
-        code_min = 'echo min' ;
+        css_path = data.css_path;
+
     // Perl
     watch.createMonitor(perl_path, function (monitor) {
       monitor.files[perl_path]
       monitor.on("created", function (f, stat) {
         console.log('Perl file or directory has been created');
-        exec("server_stop", serverTask);
+        exec(server_stop, serverTask);
+        exec(server_start, serverTask);
 
       })
       monitor.on("changed", function (f, curr, prev) {
         console.log('Perl file or directory has been changed');
-        exec("server_stop", serverTask);
+        exec(server_stop, serverTask);
+        exec(server_start, serverTask);
       })
       monitor.on("removed", function (f, stat) {
         console.log('Perl file or directory has been removed');
-        exec("server_stop", serverTask);
+        exec(server_stop, serverTask);
+        exec(server_start, serverTask);
       })
     })
     // JS
     watch.createMonitor(javascript_path, function (monitor) {
+      var code_min = data.minify;
       monitor.files[javascript_path]
       monitor.on("created", function (f, stat) {
         console.log('JS File has been created');
-        console.log(f);
-         console.log(stat);
+        exec(code_min, serverTask);
       })
       monitor.on("changed", function (f, curr, prev) {
         console.log('JS File has been changed');
-        console.log(f);
+        exec(code_min, serverTask);
       })
       monitor.on("removed", function (f, stat) {
         console.log('JS File has been removed');
+        exec(code_min, serverTask);
       })
     })
     // // CSS
     watch.createMonitor(css_path, function (monitor) {
+      var code_min = data.minify ;
       monitor.files[css_path]
       monitor.on("created", function (f, stat) {
         console.log('CSS File has been created');
-        console.log(f);
-         console.log(stat);
+        exec(code_min, serverTask);
       })
       monitor.on("changed", function (f, curr, prev) {
         console.log('CSS File has been changed');
-        console.log(f);
+        exec(code_min, serverTask);
       })
       monitor.on("removed", function (f, stat) {
         console.log('CSS File has been removed');
+        exec(code_min, serverTask);
       })
     })
 
